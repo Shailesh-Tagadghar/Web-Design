1c93f53b18c0eb2a4980ed3f35ab38b1c4c41a22
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-02-17 16:13:30 +0530

    index.js
    Var, Let and Const Keyword

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,37 @@
 document.getElementById('name_change').innerHTML="Shailesh T";
+
+// document.getElementById('name_change').innerHTML="Shailesh T";
+
+///////////////////////// VAR ///////////////////////////////////////////////
+
+// var a = 10;
+// var b = 20;
+
+// document.write(a+b);
+
+// a = a + 50;
+// document.write(a+b);
+
+// a = 100;
+// b = 20;
+
+// document.write(a+b);
+
+//////////////////// LET //////////////////////////////////////////////////
+
+// let a = 10;
+// let b = 20;
+
+// document.write(a+b);
+
+
+//////////////////// CONST //////////////////////////////////////////////////
+
+const a = 10;
+const b = 20;
+
+// document.write(a+b);
+
+a = a + 20;
+
+document.write(a+b); // this will not print anyting because we are using Const Variable so we can't update and re-declare

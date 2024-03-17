1089f3348de82826a85ca276d1ba294b6e214716
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-17 15:24:44 +0530

    Create conditional.js

---
+++ Js/conditional.js
@@ -0,0 +1,26 @@
+let a =  10;
+
+//If statement
+if(a >= 10){
+    console.log('It is True');
+
+//if-Else statement
+if(a >= 10){
+    console.log('It is True');
+}else{
+    console.log('It is False');
+}
+
+//Eligible for vote or not using if-Else
+let age = 18;
+var hasVoterId = 'Na';
+
+if(age >= 18 && hasVoterId == 'yes'){
+    console.log('Eligible to vote');
+}
+else if(age >= 18 && hasVoterId != 'yes'){
+    console.log('Apply for voterId');
+}
+else{
+    console.log('not Eligible to vote');
+}

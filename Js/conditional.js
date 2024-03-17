46e49672891c44435ba08c1805bdad7b04f177c5
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-17 16:05:29 +0530

    Update conditional.js

--- Js/conditional.js
+++ Js/conditional.js
@@ -53,5 +53,17 @@ if (age >= 18) {
     console.log('not Eligible to vote');
 }
 
-//
+// NEw Way to solve this program if age is less than 18 then it will not ask hasVoterId question
+let age = parseInt(prompt("Please enter your age"));
+if (age >= 18) {
+    hasVoterId = prompt("Do you have voterId");
+    if (hasVoterId == 'yes' || hasVoterId == 'YES' || hasVoterId == 'Yes') {
+        console.log('Eligible to vote');
+    }
+    else {
+        console.log('Apply for voterId');
+    }
+}else {
+    console.log('not Eligible to vote');
+}
 

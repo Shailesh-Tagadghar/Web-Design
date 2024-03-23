268a1c0f00ed27f47b0322dd8a0f8a84c4e3a0a8
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-23 16:11:58 +0530

    Create loops.js

---
+++ Js/loops.js
@@ -0,0 +1,12 @@
+loop :- sometime we need to run block of code again and again on that time we will use loop
+
+there are two types : 
+   1. for
+   2. while
+
+for loop : for loop which is use when we know the number of iteration
+-----------we know the how many times the loop will be repeated
+
+while loop : while loop which is used when we dont know the number of iteration
+----------while loop block will be execute infinite number of time until condition is met
+

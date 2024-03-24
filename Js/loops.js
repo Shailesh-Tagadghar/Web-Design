e0b840d72c0da8637f2a2102b2e0c0854140fc2f
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-24 15:11:45 +0530

    Update loops.js

--- Js/loops.js
+++ Js/loops.js
@@ -43,3 +43,12 @@ while(i<=10){
 console.log(i);
 =============================
 
+nested for loop examples :
+
+for(i=1;i<=3;i++){
+    for(j=1;j<=3;j++){
+        console.log(i + ' ' + j);
+    }
+}
+=============================
+

d7c88b835e9cfe075fa42a589bf32c0e57c5e3dd
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-31 16:12:37 +0530

    Update function.js

--- Js/function.js
+++ Js/function.js
@@ -1,1 +1,13 @@
-Function :-  Function is block of code, which perform a specific task it runs only when it is called.
+function :-  Function is block of code, which perform a specific task it runs only when it is called.
+
+argument :- a variable which is present in function called as argument
+
+parameter :- a varibale is present as function header part is called as parameter
+eg :
+function add(a,b){ ---parameter
+  console.log(a+b);
+}
+a=10;
+b=20;
+add(a+b); ---argument
+
